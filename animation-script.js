3d00fbc9a8495081e935e9e97f01c1f6d512a67a
ferrar16 <ferrar16@msu.edu>
2017-02-13 01:22:15 -0500

    Everything is changing, except for war, that never changes.

---
+++ animation-script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+	$('.satellites').mouseenter(function() {
+		$('.s1').addClass("stop-animation");
+		$('.s2').addClass("stop-animation");
+		$('.s3').addClass("stop-animation");
+		$('.s4').addClass("stop-animation");
+		$('.s5').addClass("stop-animation");
+		$('.s6').addClass("stop-animation");
+		$('.s7').addClass("stop-animation");
+		$('.s8').addClass("stop-animation");
+		$('.s9').addClass("stop-animation");
+		$('.s10').addClass("stop-animation");
+		$('.s11').addClass("stop-animation");
+		$('.s12').addClass("stop-animation");	
+	});
+
+	$('.satellites').mouseleave(function() {
+		$('.s1').removeClass("stop-animation");
+		$('.s2').removeClass("stop-animation");
+		$('.s3').removeClass("stop-animation");
+		$('.s4').removeClass("stop-animation");
+		$('.s5').removeClass("stop-animation");
+		$('.s6').removeClass("stop-animation");
+		$('.s7').removeClass("stop-animation");
+		$('.s8').removeClass("stop-animation");
+		$('.s9').removeClass("stop-animation");
+		$('.s10').removeClass("stop-animation");
+		$('.s11').removeClass("stop-animation");
+		$('.s12').removeClass("stop-animation");	
+	});
+
+})
